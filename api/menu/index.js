05019f30e0cb2905fb452d59634d71c9e79fd8cc
SmartBro <smartbrostudio@gmail.com>
2016-11-14 22:08:09 +0200

    add fake response for menu

--- api/menu/index.js
+++ api/menu/index.js
@@ -1,9 +1,26 @@
 const menuRouter = require('express').Router();
+const faker = require('faker');
 
 menuRouter.get('/', getMenuList);
 
 module.exports = menuRouter;
 
-function getMenuList(req, res) {
-  return res.json({menu: [ 'First Dish', 'Second Dish' ]});
+function getMenuList (req, res) {
+  const fakeMenus = getFakeMenus(20);
+  return res.json({menu: fakeMenus});
+}
+
+function getFakeMenus (count) {
+  const fakeMenus = [];
+  for (let i = 0; i < count; i++) {
+    const fakeMenu = {
+      id: faker.random.uuid(),
+      name: faker.commerce.productName(),
+      author: faker.name.findName(),
+      date: faker.date.past()
+    };
+    fakeMenus.push(fakeMenu);
+  }
+
+  return fakeMenus;
 }
