49790772217c35222f120178c8f75ca0c33176cc
SmartBro <smartbrostudio@gmail.com>
2016-11-28 20:00:05 +0200

    add single recipe api route

--- api/api-mocks/recipes.js
+++ api/api-mocks/recipes.js
@@ -1,21 +1,26 @@
 const faker = require('faker');
 
 module.exports = {
+  getFakeRecipe,
   getFakeRecipes
 };
+
+function getFakeRecipe (id) {
+  return {
+    id,
+    name: faker.commerce.productName(),
+    ingredients: faker.lorem.lines(),
+    method: faker.lorem.paragraphs(),
+    image: faker.image.food(),
+    author: faker.name.findName(),
+    date: faker.date.past()
+  };
+}
 
 function getFakeRecipes (count) {
   const fakeRecipes = [];
   for (let i = 0; i < count; i++) {
-    const fakeRecipe = {
-      id: faker.random.uuid(),
-      name: faker.commerce.productName(),
-      ingredients: faker.lorem.lines(),
-      method: faker.lorem.paragraphs(),
-      image: faker.image.food(),
-      author: faker.name.findName(),
-      date: faker.date.past()
-    };
+    const fakeRecipe = getFakeRecipe(faker.random.uuid());
     fakeRecipes.push(fakeRecipe);
   }
 
--- api/recipes/index.js
+++ api/recipes/index.js
@@ -1,10 +1,15 @@
 const recipesRouter = require('express').Router();
-const { getFakeRecipes } = require('../api-mocks/recipes');
+const { getFakeRecipe, getFakeRecipes } = require('../api-mocks/recipes');
 
 recipesRouter.get('/', getRecipesList);
+recipesRouter.get('/:recipeId', getRecipeById);
 
 module.exports = recipesRouter;
 
 function getRecipesList(req, res) {
   return res.json({recipes: getFakeRecipes(20)});
+}
+
+function getRecipeById(req, res) {
+  return res.json(getFakeRecipe(req.params.recipeId));
 }
