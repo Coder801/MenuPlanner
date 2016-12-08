95f2ae705efa31544f7c7921587fefcd1ae42cbb
SmartBro <smartbrostudio@gmail.com>
2016-12-08 23:26:37 +0200

    fix requests with body

--- src/shared/requests.js
+++ src/shared/requests.js
@@ -1,9 +1,16 @@
 const baseApiUrl = '/api/';
+const headers = {
+  'Content-type': 'application/json; charset=UTF-8'
+};
 
 export const [ read, create, update, remove ] = [ 'GET', 'POST', 'PUT', 'DELETE' ].map(createMethod);
 
 function createMethod (method) {
-  return (url, body = {}) => fetch(`${baseApiUrl}${url}`, { method, body }).then(getJson);
+  return (url, body) => {
+    return fetch(`${baseApiUrl}${url}`,
+      { method, headers, body: JSON.stringify(body) }
+    ).then(getJson);
+  };
 }
 
 function getJson(response) {
