9a0d6090d3838bc23eacb646481989e7fd9f8432
SmartBro <smartbrostudio@gmail.com>
2017-01-05 00:08:46 +0200

    add simple component test

--- src/components/recipes/recipe-details/recipe-details.spec.js
+++ src/components/recipes/recipe-details/recipe-details.spec.js
@@ -1,3 +1,14 @@
-it('[Recipe Details]', () => {
-  expect([ 'test' ].length).toBe(1);
+import { RecipeDetails } from './recipe-details';
+import renderer from 'react-test-renderer';
+
+const mockParams = {
+  recipeId: '123'
+};
+
+it('[Recipe Details] shows preloader when recipe === null', () => {
+  const component = renderer.create(
+    <RecipeDetails recipe={null} params={mockParams} loadRecipe={jest.fn()} />
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
